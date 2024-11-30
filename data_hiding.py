b307182ad3328fa997fcf8f367b20b9bf9806107
mirzahaseebbaig <mirza.baig97@hotmail.com>
2024-12-01 01:41:32 +0500

    __ methods and __ properties.

--- data_hiding.py
+++ data_hiding.py
@@ -2,6 +2,7 @@ class Product:
     def __init__(self):
         self.data1 = 10
         self._data2 = 20 #programer should respect that variable which starts with _ underscore in python and don't change.
+        self.__data3 = 30 #you can not directly call this.
 
     def methodA(self):
         pass
@@ -9,5 +10,11 @@ class Product:
     def _methodB(self): #programer should respect that method which starts with _ underscore in python and don't change.
         pass
 
+    def __methodC(self): #you can not directly call this method.
+        pass
+
 
 p = Product()
+
+# p.__methodC() it will throw error, use dir(p) to see the hidden name and call via that name. ex -> _Product__methodC()
+p._Product__methodC()
